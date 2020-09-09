9e6b0e13f21909dac98c53ca834dc5d060d0a38d
Alexander Rose <acrosemail@gmail.com>
2020-09-09 11:56:16 -0500

    AaronCarter
    Thats how I beat shaq

---
+++ AaronCarter/beatShaq.sh
@@ -0,0 +1,4 @@
+		function beatShaq() {
+			 say -f /Users/Alex/Desktop/Workflows/AppleAutomation/AaronCarter/HowIBeatShaq.txt | \
+			 mplayer /Users/Alex/Desktop/Workflows/AppleAutomation/AaronCarter/HowIBeatShaq.mp3
+	}
