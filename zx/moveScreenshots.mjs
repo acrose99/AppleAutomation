791e8778144c41ceda8b68650c4b6b2b643ee59f
Alexander Rose <acrbrazil99@gmail.com>
2021-06-23 20:35:56 -0500

    Fix moveScreenshots
    now it works for any directory

--- zx/moveScreenshots.mjs
+++ zx/moveScreenshots.mjs
@@ -1,7 +1,15 @@
 #!/usr/bin/env zx
 
 /* Change path to what you want, I'm using the Desktop, the default for MACOS */
-let files = await fs.readdir('../', (err, files) => {
+
+
+var process = require('process');
+
+process.chdir(os.homedir() + '/Desktop');
+
+console.log(chalk.magenta("Getting Screenthots from:" + process.cwd() + "... \n"));
+
+let files = await fs.readdir('../Desktop', (err, files) => {
     if (err) {
         throw err
     }
@@ -10,10 +18,11 @@ let files = await fs.readdir('../', (err, files) => {
     }
 })
 
-let screenshots = await files.filter(file => file.includes("Screenshot") && file.includes("~lock") == false)
-console.log(chalk.blue("Moving the following files:"));
-console.log(chalk.magenta(screenshots));
+let screenshots = await files.filter(file => file.includes("Screen Shot") && file.includes("~lock") == false)
+console.log(chalk.cyan("Moving the following files:"));
+// console.log(chalk.magenta(screenshots));
 await screenshots.forEach(file => {
-    fs.rename("../" + file, "../Images/Screenshots/" + file, function (err) {
+    console.log(chalk.hex('#93f5b8')(file) + '\n');
+    fs.rename("../Desktop/" + file, "../Desktop/Images/Screenshots/" + file, function (err) {
         }
-    )})
+)})
